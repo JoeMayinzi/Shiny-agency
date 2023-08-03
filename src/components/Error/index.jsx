35a7b16b2d2b20b039d2be139644b8f89220555f
Joemayinzi <joemayinzi@gmail.com>
2023-08-03 19:02:39 +0100

    create 404 page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { styled } from "styled-components";
+import colors from "../../utils/style/colors";
+import ErrorImg from "../../assets/undraw_page_not_found_su7k 1.svg";
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.backgroundLight};
+  align-items: center;
+`;
+
+const ErrorTitle = styled.h1`
+  font-weight: 300;
+`;
+
+const ErrorSubtitle = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`;
+
+const Illustration = styled.img`
+  max-width: 800px;
+`;
 
 const Error = () => {
   return (
-    <div>
-      <h1>Oups 🙈 Cette page n'existe pas</h1>
-    </div>
+    <ErrorWrapper>
+      <ErrorTitle>Oups...</ErrorTitle>
+      <Illustration src={ErrorImg} />
+      <ErrorSubtitle>
+        Il semblerait que la page que vous cherchez n’existe pas
+      </ErrorSubtitle>
+    </ErrorWrapper>
   );
 };
 
